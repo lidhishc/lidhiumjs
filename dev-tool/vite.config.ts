038530f6308a04b740123de957cfea3c68c21e93
lidhishc <lidhishc@gmail.com>
2025-03-26 17:55:55 +0530

    updated config

--- dev-tool/vite.config.ts
+++ dev-tool/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    outDir: "build",
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: true,
+  },
+});
