dcaefa6142302fa0b14c1b3f6ee829215c81a156
lidhishc <lidhishc@gmail.com>
2025-03-30 13:20:47 +0530

    tailwind config updated

--- src/static/config_files/tailwind.config.js
+++ src/static/config_files/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./apps/**/*.{vue,jsx,tsx}",
+    "./apps/**/*.vue",
+    "./global/**/*.{vue,jsx,tsx}",
+    "./global/**/*.vue",
+  ],
   theme: {
     extend: {},
   },
